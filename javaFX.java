645db81a1e0a2f86dc721b7df99c20ba71bfa006
MVBeats <real9ms@gmail.com>
2023-11-27 03:14:28 +0100

    updated connection to Server with Username

--- javaFX.java
+++ javaFX.java
@@ -9,14 +9,20 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import io.socket.client.SocketOptionBuilder;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.*;
 
 public class javaFX extends Application {
     private Socket socket;
     private Label ausgabeGUI;
+    final int width = 1280;
+    final int height = 960;
+    String username;
     @Override
     public void start(Stage primaryStage) {
         ausgabeGUI = new Label();
@@ -42,7 +48,6 @@ public class javaFX extends Application {
         primaryStage.setScene(scene);
     
         primaryStage.show();
-    
     }
 
     private void establishConnection() {
@@ -50,8 +55,12 @@ public class javaFX extends Application {
             //Testweise Socket IO Chat Server
             //socket = IO.socket("https://socketio-chat-h9jt.herokuapp.com/");
             //Unser Server
-            socket = IO.socket("3.71.101.250:3000");
-    
+            String username = "IhrBenutzername";
+            IO.Options options = new IO.Options();  
+            options = IO.Options.builder().setExtraHeaders(Collections.singletonMap("username", Collections.singletonList(username))).build();
+
+            socket = IO.socket("http://3.71.101.250:3000/",options);
+
             socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
                 @Override
                 public void call(Object... args) {
